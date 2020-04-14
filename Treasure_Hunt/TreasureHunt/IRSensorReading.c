99424cf1330457f24852e65c1f922b906bc573d7
Tobias <tobias.michel@epfl.ch>
2020-04-14 17:46:54 +0200

    finished part tobias of the Proxythread.

--- Treasure_Hunt/TreasureHunt/IRSensorReading.c
+++ Treasure_Hunt/TreasureHunt/IRSensorReading.c
@@ -1,2 +1,99 @@
-#include <main.h>
-#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include "ch.h"
+#include "hal.h"
+#include <stdlib.h>
+#include "main.h"
+#include <chprintf.h>
+
+#include "sensors/proximity.h"
+
+#include "IRSensorReading.h"
+
+
+//***defines
+#define SENSORTRIGGERVALUE 500
+#define SENSORISTIRGGERED 1
+
+
+//*****constants
+const int8_t IRSensorDegree[PROXIMITY_NB_CHANNELS]={15,45,90,110,-110,-90,-45,-15};
+
+
+
+
+//********static variables
+
+
+static IRData processingValues;
+
+static THD_WORKING_AREA(IRSensorProcessing_wa, 512);
+static THD_FUNCTION(IRSensorProcessing, arg) {
+     (void) arg;
+     chRegSetThreadName(__FUNCTION__);
+
+     messagebus_topic_t *IRValuesTopic;
+     IRValuesTopic=messagebus_find_topic(&bus, "/proximity");
+     proximity_msg_t irValues;
+     while(true)
+     {
+    	 messagebus_topic_wait(IRValuesTopic, &irValues, sizeof(irValues));
+    	 uint8_t nbOfTriggeredSensors=0;
+    	 processingValues.directionOfObstacle=0;
+    	 for(uint8_t sensorCounter=0;sensorCounter<PROXIMITY_NB_CHANNELS;sensorCounter++)
+    	 {
+    		 if(irValues.delta[sensorCounter]>SENSORTRIGGERVALUE)
+    		 {
+    			 processingValues.directionOfObstacle+=IRSensorDegree[sensorCounter];
+    			 processingValues.obstaclePresent=true;
+    			 nbOfTriggeredSensors++;
+    		 }
+
+    	 }
+    	 if(nbOfTriggeredSensors==0)
+    	 {
+    		 processingValues.obstaclePresent=false;
+    	 }
+
+    	 processingValues.directionOfObstacle= processingValues.directionOfObstacle/nbOfTriggeredSensors;
+    	 if(processingValues.obstaclePresent && processingValues.ignoringObstacle==false)
+    	 {
+    		 set_front_led(1);
+    	 }
+    	 else
+    	 {
+    		 set_front_led(0);
+    	 }
+
+    	 //chprintf((BaseSequentialStream *)&SD3,"Published present: %s, Value: %d\n\r",processingValues.obstaclePresent ? "true" : "false",processingValues.directionOfObstacle);
+
+     }
+
+
+
+}
+
+
+
+
+
+
+//***************Public functions *****************************************
+void IRProcessingStart(void)
+{
+	proximity_start();
+	chThdCreateStatic(IRSensorProcessing_wa, sizeof(IRSensorProcessing_wa), NORMALPRIO, IRSensorProcessing, NULL);
+
+}
+bool isObstaclePresent(void)
+{
+	return processingValues.obstaclePresent;
+}
+int16_t getObstacleDirection(void)
+{
+	return processingValues.directionOfObstacle;
+}
+void ignoreObstacle(bool doIgnoreObstacle)
+{
+	processingValues.ignoringObstacle=doIgnoreObstacle;
+}
--- Treasure_Hunt/TreasureHunt/IRSensorReading.h
+++ Treasure_Hunt/TreasureHunt/IRSensorReading.h
@@ -1,7 +1,20 @@
 #ifndef IRSENSOREADING_H
 #define IRSENSOREADING_H
+#include "msgbus/messagebus.h"
 
+typedef struct {
+	bool obstaclePresent;
+	bool ignoringObstacle;
+	int16_t directionOfObstacle;
 
+}IRData;
+
+extern messagebus_t bus;
+
+void IRProcessingStart(void);
+bool isObstaclePresent(void);
+int16_t getObstacleDirection(void);
+void ignoreObstacle(bool doIgnoreObstacle);
 
 #endif
 
