95d823cbe030b49071f8eb8bab542eeb3a57bcdf
Tobias <tobias.michel@epfl.ch>
2020-04-16 18:33:39 +0200

    first version of motorfunctionality implemented

--- Treasure_Hunt/TreasureHunt/driveMotors.h
+++ Treasure_Hunt/TreasureHunt/driveMotors.h
@@ -2,5 +2,14 @@
 #define DRIVEMOTORS_H
 
 
+float wrapAngle(float angle);
+void setDesiredBearing(float desiredBearing);
+void setDesiredSpeed(int16_t speed);
+float getCurrentBearing(void);
+void startMotors(void);
+float getXPosition(void);
+float getYPosition(void);
+void isAllowedToDrive(bool doDrive);
+
 
 #endif /* DRIVEMOTORS_H */
